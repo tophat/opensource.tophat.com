c045992f116b2b982f492b91882e1c528de0e08d
Sanchit Gera <gerasanchit@gmail.com>
2018-11-22 15:10:19 -0500

    Portal V1 (#24)
    * Portal V1
    
    * Make website responsive
    
    * Move static files to separate directory
    
    * Remove ctags
    
    * Add slack integration
    
    * Update sanity runner description:
    
    * Fix form validation

---
+++ static/js/slack.js
@@ -0,0 +1,80 @@
+const validateEmail = (email) => {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase())
+}
+
+const showCheckmark = () => {
+    const element = document.getElementsByClassName('slack-success')[0]
+    element.style.visibility = 'visible'
+}
+
+const showFormValidation = messageType => {
+    let element
+    switch(messageType) {
+        case 'success' :
+            showCheckmark()
+            element = document.getElementsByClassName('form-submit-message success')[0]
+            break
+        case 'resubmit':
+            element = document.getElementsByClassName('form-submit-message resubmit')[0]
+            break
+        case 'error':
+        default:
+            element = document.getElementsByClassName('form-submit-message error')[0]
+    }
+    element.style.visibility = 'visible';
+}
+
+const hideMessages = () => {
+    const elements = document.getElementsByClassName('form-submit-message')
+    for(let i = 0; i < elements.length; i++){ 
+        element = elements[i]
+        element.style.visibility = 'hidden'
+    }
+    const check = document.getElementsByClassName('slack-success')[0]
+    check.style.visibility = 'hidden'
+}
+
+const resubmittingRequest = (message) => {
+    return message.indexOf('You have already been invited to Slack') > -1
+}
+
+const handleFormSubmit = (e) => {
+    e.preventDefault()
+    hideMessages()
+    const email = document.getElementById('email').value
+    if(!email) return
+
+    const isValid = validateEmail(email)
+    if(!isValid){
+        const element = document.getElementsByClassName('form-submit-message invalid')[0]
+        element.style.visibility = 'visible';
+        return
+    }    
+
+    var http = new XMLHttpRequest();
+    var url = 'https://slackinvite.dev.tophat.com/invite';
+    var params = JSON.stringify({ email })
+
+    http.open('POST', url, true);
+    http.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+      http.onreadystatechange = function() {
+        if (http.readyState === 4) {
+            const response = JSON.parse(http.response)
+            if(response.success) {
+                showFormValidation('success')
+            } else if(resubmittingRequest(response.message)) {
+                showFormValidation('resubmit')
+            } else {
+                showFormValidation('error')
+            }
+        }
+      }
+    http.send(params);
+}
+
+
+const form = document.getElementById('slack-invite')
+const submitButton = document.getElementById('slack-submit-button')
+form.addEventListener('submit', handleFormSubmit)
+submitButton.addEventListener('click', handleFormSubmit)
