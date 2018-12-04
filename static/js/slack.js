88e080b3fb252869b8c7d0881275930d848cfc94
Jake Bolam <jake.bolam@gmail.com>
2018-12-04 17:11:47 -0500

    feat: if already signed up, let them know about the workspace (#52)
    * let users know if they're already in the workspace
    
    * let users know if they're already in the workspace

--- static/js/slack.js
+++ static/js/slack.js
@@ -18,6 +18,9 @@ const showFormValidation = messageType => {
         case 'resubmit':
             element = document.getElementsByClassName('form-submit-message resubmit')[0]
             break
+        case 'exists':
+            element = document.getElementsByClassName('form-submit-message exists')[0]
+            break
         case 'error':
         default:
             element = document.getElementsByClassName('form-submit-message error')[0]
@@ -39,17 +42,27 @@ const resubmittingRequest = (message) => {
     return message.indexOf('You have already been invited to Slack') > -1
 }
 
+const existsRequest = (message) => {
+    return message.indexOf('Sending you to Slack...') > -1
+}
+
 const handleFormSubmit = (e) => {
     e.preventDefault()
     hideMessages()
-    const email = document.getElementById('email').value
+    const emailNode = document.getElementById('email')
+    const email = emailNode.value
 
     const isValid = email && validateEmail(email)
     if(!isValid){
         const element = document.getElementsByClassName('form-submit-message invalid')[0]
         element.style.visibility = 'visible';
+        emailNode.focus()
         return
     }
+
+    const buttonNode = document.getElementById('slack-submit-button')
+    buttonNode.classList.add('disabled')
+    buttonNode.disabled = true
 
     var http = new XMLHttpRequest();
     var url = 'https://slackinvite.dev.tophat.com/invite';
@@ -59,13 +72,20 @@ const handleFormSubmit = (e) => {
     http.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
       http.onreadystatechange = function() {
         if (http.readyState === 4) {
+            buttonNode.classList.remove('disabled')
+            buttonNode.disabled = false
             const response = JSON.parse(http.response)
             if(response.success) {
                 showFormValidation('success')
             } else if(resubmittingRequest(response.message)) {
                 showFormValidation('resubmit')
+                emailNode.focus()
+            } else if(existsRequest(response.message)) {
+                showFormValidation('exists')
+                emailNode.focus()
             } else {
                 showFormValidation('error')
+                emailNode.focus()
             }
         }
       }
