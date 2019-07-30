85b0596329bbb30fb1743ddd58b7d0adc22a210b
Marc Cataford <marc.cataford@tophatmonocle.com>
2019-07-29 20:47:21 -0400

    Better use of gazers stats on project cards (#62)
    * wip: draft, sort by gazers
    
    * wip: sort by pushed_at
    
    * wip: clean up
    
    * cr: whitespace
    
    Co-Authored-By: Emmanuel Ogbizi <iamogbz+github@gmail.com>
    
    * cr: array constructor
    
    Co-Authored-By: Emmanuel Ogbizi <iamogbz+github@gmail.com>
    
    * cr: string style
    
    Co-Authored-By: Emmanuel Ogbizi <iamogbz+github@gmail.com>

--- static/js/stars.js
+++ static/js/stars.js
@@ -3,33 +3,61 @@ function fetchRepoDataBlob() {
     let http = new XMLHttpRequest()
     http.open('GET', endpoint, true)
     http.onreadystatechange = function() {
-        if (http.readyState === 4) {
+        if (http.readyState !== 4) return
+
+        if (http.status === 200 && http.response) {
             const responseData = JSON.parse(http.response)
             const starCount = gatherStargazerData(responseData)
             insertStargazerBadges(starCount)
+        } else {
+            disableStargazerBadges()
         }
     }
     http.send()
 }
 
+function _sortByStarCount(gazers) {
+    return Object.entries(gazers).sort(function(first, second) {
+        const firstCount = first[1].stars
+        const secondCount = second[1].stars
+        return firstCount - secondCount
+    })
+}
+
+function _sortByLastPush(gazers) {
+    return Object.entries(gazers).sort(function(first, second) {
+        const firstActive = new Date(first[1].active)
+        const secondActive = new Date(second[1].active)
+        if (firstActive < secondActive) return 1
+        else if (firstActive > secondActive) return -1
+        else return 0
+    })
+}
+
 function gatherStargazerData(response) {
-    return response.reduce(function (acc, project) {
-        const { name, stargazers_count: stars } = project
-        acc[name] = stars
+    const gazers = response.reduce(function (acc, project) {
+        const { name, stargazers_count: stars, pushed_at:active } = project
+        acc[name] = { stars, active }
         return acc
     }, {})
+    return _sortByLastPush(gazers)
 }
 
 function insertStargazerBadges(starCounts) {
-    Object.entries(starCounts).forEach(function([project, item]) {
-        const badge = document.querySelector(".project-stars[data-project='" + project +"']")
-
+    starCounts.forEach(function([project, item], index) {
+         const badge = document.querySelector(`.project-stars[data-project='${project}']`)
         if (badge) {
+            const projectBox = badge.parentNode
             const count = badge.querySelector('.count')
-            count.innerText = item
+            count.innerText = item.stars
             badge.style.opacity = 1
+            projectBox.style.order = index
         }
     })
+}
+
+function disableStargazerBadges() {
+   [...document.getElementsByClassName('project-stars')].forEach(item => item.remove())
 }
 
 fetchRepoDataBlob()
